d65c450f306db053eeb6ce7461db5103375b4259
fonsinchen <fonsinchen@fickzoo.com>
2012-03-31 03:55:59 +0200

    make tabbed chat somewhat functional

--- kwarque/yakity/js/tabbedChat.js
+++ kwarque/yakity/js/tabbedChat.js
@@ -15,51 +15,55 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-psyc.TabbedRoom = function(templates, id) {
-	psyc.RoomWindow.call(this, templates, id);
-	this.li = document.createElement("li");
-	// hide all by default
-	this.container = document.createElement("div");
-	this.li.className = "chatheader roomchat header_hidden";
-	this.container.className = "chatwindow roomchat window_hidden";
-	this.container.appendChild(this.messages);
-	this.container.appendChild(this.getMembersNode());
-	this.active = null;
-};
-psyc.TabbedRoom.prototype = new psyc.RoomWindow();
-psyc.TabbedRoom.prototype.initialize = psyc.TabbedRoom;
-psyc.TabbedRoom.prototype.hide = function() {
-	this.li.className = this.li.className.replace("header_shown", "header_hidden");
-	this.container.className = this.container.className.replace("window_shown", "window_hidden");
-};
-psyc.TabbedRoom.prototype.show = function() {
-	this.li.className = this.li.className.replace("header_hidden", "header_shown");
-	this.container.className = this.container.className.replace("window_hidden", "window_shown");
-};
-psyc.TabbedPrivate = function(templates, id) {
-	psyc.TemplatedWindow.call(this, templates, id);
-	this.li = document.createElement("li");
-	// hide all by default
-	this.container = document.createElement("div");
-	this.li.className = "chatheader privatechat header_hidden";
-	this.container.className = "chatwindow privatechat window_hidden";
-	this.container.appendChild(this.messages);
-	this.active = null;
-};
-psyc.TabbedPrivate.prototype = new psyc.TemplatedWindow();
-psyc.TabbedPrivate.prototype.initialize = psyc.TabbedPrivate;
-psyc.TabbedPrivate.prototype.hide = function() {
-	this.li.className = this.li.className.replace(/header_shown/g, "header_hidden");
-	this.container.className = this.container.className.replace(/window_shown/g, "window_hidden");
-};
-psyc.TabbedPrivate.prototype.show = function() {
-	this.li.className = this.li.className.replace(/header_hidden/g, "header_shown");
-	this.container.className = this.container.className.replace(/window_hidden/g, "window_shown");
-};
-psyc.TabbedChat = new Class({
-	Extends : psyc.Chat,
-	initialize : function(client, templates) {
-		psyc.Chat.call(this, client);
+Yakity.TabbedRoom = new Class({
+	Extends : Yakity.RoomWindow,
+	initialize : function(templates, id) {
+		this.parent(templates, id);
+		this.header = document.createElement("li");
+		// hide all by default
+		this.container = document.createElement("div");
+		this.header.className = "chatheader roomchat header_hidden";
+		this.container.className = "chatwindow roomchat window_hidden";
+		this.container.appendChild(this.messages);
+		this.container.appendChild(this.getMembersNode());
+		this.active = null;
+	},
+	hide : function() {
+		this.header.className = this.header.className.replace("header_shown", "header_hidden");
+		this.container.className = this.container.className.replace("window_shown", "window_hidden");
+	},
+	show : function() {
+		this.header.className = this.header.className.replace("header_hidden", "header_shown");
+		this.container.className = this.container.className.replace("window_hidden", "window_shown");
+	}
+});
+
+Yakity.TabbedPrivate = new Class({
+	Extends : Yakity.TemplatedWindow,
+	initialize : function(templates, id) {
+		this.parent(templates, id);
+		this.header = document.createElement("li");
+		// hide all by default
+		this.container = document.createElement("div");
+		this.header.className = "chatheader privatechat header_hidden";
+		this.container.className = "chatwindow privatechat window_hidden";
+		this.container.appendChild(this.messages);
+		this.active = null;
+	},
+	hide : function() {
+		this.header.className = this.header.className.replace(/header_shown/g, "header_hidden");
+		this.container.className = this.container.className.replace(/window_shown/g, "window_hidden");
+	},
+	show : function() {
+		this.header.className = this.header.className.replace(/header_hidden/g, "header_shown");
+		this.container.className = this.container.className.replace(/window_hidden/g, "window_shown");
+	}
+});
+
+Yakity.TabbedChat = new Class({
+	Extends : Yakity.Chat,
+	initialize : function(client, templates, target_id) {
+		this.parent(client);
 
 		if (!client) return;
 
@@ -69,6 +73,8 @@ psyc.TabbedChat = new Class({
 		this.ul.className = "chatheader";
 		this.div = document.createElement("div");
 		this.div.className = "chatcontainer";
+		document.getElementById(target_id).appendChild(this.ul);
+		document.getElementById(target_id).appendChild(this.div);
 	},
 	/**
 	 * Opens a new tab inside the chat. It will be used to display messages coming from the entity specified by uniform. Use this for cases like private messages where the conversation is not initiated by a handshake (in contrary to group chats).
@@ -78,9 +84,9 @@ psyc.TabbedChat = new Class({
 		var win;
 
 		if (uniform.is_room()) {
-			win = new psyc.TabbedRoom(this.templates, uniform);
+			win = new Yakity.TabbedRoom(this.templates, uniform);
 		} else {
-			win = new psyc.TabbedPrivate(this.templates, uniform);
+			win = new Yakity.TabbedPrivate(this.templates, uniform);
 		}
 
 		if (!this.active) {
@@ -89,11 +95,11 @@ psyc.TabbedChat = new Class({
 		}
 
 		var self = this;
-		win.li.onclick = function(event) {
+		win.header.onclick = function(event) {
 			self.activateWindow(uniform);
 		};
 
-		this.ul.appendChild(win.li);
+		this.ul.appendChild(win.header);
 		this.div.appendChild(win.container);
 
 		return win;
